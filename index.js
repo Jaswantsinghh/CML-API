79f83c9128dc388a47200bca3cd4fc669e4e2cbc
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-08-27 17:43:59 +0530

    external files added

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const createEvent=require('./routes/event');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname+'/public')));
 app.use('/', createEvent);
+app.get('/show-events',(req,res)=>{
+  res.sendFile(path.join(__dirname+'/public/events.html'));
+});
 app.get('*',function(req, res){
-  res.status(400).send('Oops!! This page do not exist :(');
+  res.status(400).send('Oops!! This page does not exist :(');
 });
 
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true,useUnifiedTopology: true },() =>{
